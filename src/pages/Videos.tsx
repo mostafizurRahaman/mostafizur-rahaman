20ff89788d9ed4ef8726e13a9d6a1ea8be1c016e
mostafizur.rahaman <mostafizur.rahaman@trainingmug.com>
2023-04-30 18:21:03 +0600

    my video route is ready to go

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
+import ReactPlayer from "react-player/youtube";
+import Headings from "../components/Headings";
 
 const Videos = () => {
    const [videos, setVideos] = useState<[]>([]);
    const [videos1, setVideos1] = useState<[]>([]);
+
+   console.log(videos);
+
    useEffect(() => {
       fetch(
-         `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=20&playlistId=${import.meta.env.VITE_WEB_PLAYLIST_ID}&key=${
-            import.meta.env.VITE_YOUTUBE_API
-         }`
+         `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=20&playlistId=${
+            import.meta.env.VITE_WEB_PLAYLIST_ID
+         }&key=${import.meta.env.VITE_YOUTUBE_API}`
       )
          .then((res) => res.json())
          .then((data) => {
@@ -18,11 +23,12 @@ const Videos = () => {
          })
          .catch((err) => console.log(err));
    }, []);
+
    useEffect(() => {
       fetch(
-         `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=${import.meta.env.VITE_WEB_PLAYLIST_ID1}&key=${
-            import.meta.env.VITE_YOUTUBE_API
-         }`
+         `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=20&playlistId=${
+            import.meta.env.VITE_WEB_PLAYLIST_ID1
+         }&key=${import.meta.env.VITE_YOUTUBE_API}`
       )
          .then((res) => res.json())
          .then((data) => {
@@ -35,36 +41,21 @@ const Videos = () => {
    }, []);
 
    return (
-      <div>
-         <h2 className="text-5xl font-bold mt-5 text-center text-secondary  ">
-            Web Development Tutorials
-         </h2>
-         <div className="grid  grid-cols-3 gap-5 items-center justify-center p-10">
+      <div className="bg-info pt-5">
+         <Headings content="Web Development Tutorials"></Headings>
+         <div className="grid  grid-cols-1 md:grid-cols-3 gap-5 items-center justify-center p-10">
             {videos?.map((i: any) => (
-               <div key={i.id}>
-                  <iframe
-                     className=" w-full h-52"
-                     src={`https://www.youtube.com/embed/${i}`}
-                     title="YouTube video player"
-                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                     allowFullScreen
-                  ></iframe>
-               </div>
-            ))}
-         </div>
-         <h2 className="text-5xl font-bold mt-5 text-center text-secondary  ">
-            ALL Tutorials
-         </h2>
-         <div className="grid  grid-cols-3 gap-5 items-center justify-center p-10">
-            {videos1?.map((i: any) => (
-               <div key={i.id}>
-                  <iframe
-                     className=" w-full h-52"
-                     src={`https://www.youtube.com/embed/${i}`}
-                     title="YouTube video player"
-                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                     allowFullScreen
-                  ></iframe>
+               <div className="p-2 rounded-lg border border-white ">
+                  <ReactPlayer
+                     className="react-player"
+                     width="100%"
+                     height="220px"
+                     key={i.id}
+                     url={`https://www.youtube.com/watch?v=${i}`}
+                     controls={true}
+                     volume={1}
+                     loop={false}
+                  />
                </div>
             ))}
          </div>
